e5c7c172dad5d47d9073f6d4c7f415e5ab8286af
David Torres <davidtorresim@gmail.com>
2023-01-09 23:18:34 -0300

    Manejo de errores con boom

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const { logErrors, errorHandler } = require('./middlewares/error.handler')
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,9 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 
 app.listen(port, () => {
